0d6bb403a89be5fd87e9e02b3e3ebbbf8dec2556
arnaldorcm <arnaldo1ooo95@gmail.com>
2021-08-15 11:18:22 -0400

    Commit inicial

---
+++ springBootPractica/src/main/java/com/practica/springBootPractica/SpringBootPracticaApplication.java
@@ -0,0 +1,13 @@
+package com.practica.springBootPractica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPracticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPracticaApplication.class, args);
+	}
+
+}
